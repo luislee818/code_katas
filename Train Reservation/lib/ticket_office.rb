61cf8a1c8113209422b30949d2769f8ebb5edfe4
Dapeng Li <luislee818@gmail.com>
2014-01-31 17:47:26 +0800

    Add specs for interaction between TicketOffice and services

--- Train Reservation/lib/ticket_office.rb
+++ Train Reservation/lib/ticket_office.rb
@@ -1,14 +1,24 @@
 module TrainReservationKata
   class TicketOffice
-    attr_writer :train_data_service
+    attr_writer :train_data_service, :seat_analyzer, :seat_reservation_service
 
     def book_seats(train_id, number_of_seats)
       train_data_service.get_train_data
+      available_seats = seat_analyzer.get_available_seats
+      seat_reservation_service.reserve_seats unless available_seats.empty?
       SeatReservationResult.new(train_id, "foo", [1, 2])
     end
 
     def train_data_service
       @train_data_service || TrainDataService.new
+    end
+
+    def seat_analyzer
+      @seat_analyzer || SeatAnalyzer.new
+    end
+
+    def seat_reservation_service
+      @seat_reservation_service || SeatReservationService.new
     end
   end
 
@@ -24,6 +34,17 @@ module TrainReservationKata
 
   class TrainDataService
     def get_train_data
+    end
+  end
+
+  class SeatAnalyzer
+    def get_available_seats
+      []
+    end
+  end
+
+  class SeatReservationService
+    def reserve_seats
     end
   end
 end
